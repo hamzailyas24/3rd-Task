fcd2f31b328de9d8db044560d8f423ab8cd9e197
Hamza Ilyas <hamza24061@gmail.com>
2021-10-13 19:05:28 +0500

    Completed

---
+++ app.js
@@ -0,0 +1,87 @@
+// Write a Table Using User Input
+
+// let table = +prompt("Enter the table number");
+// let length = +prompt("Enter the length how many time to print");
+// for (var i = 1; i <= length; i++) {
+//     document.write(table + " X " + i + " = " + table * i + "<br>");
+// }
+
+// Write a program to print items of the following array using for loop
+
+// var fruits=["Apple", "Banana", "Mango", "Orange", "Strawberry"];
+
+// for(var i=0; i<fruits.length; i++){
+//     document.write(fruits[i] + "<br>");
+// }
+
+// for(var i=0; i<fruits.length; i++){
+//     document.write("<b> Element at Index </b> " + fruits.indexOf(fruits[i]) + " is " + fruits[i] + "<br>");
+// }
+
+// Print Counting 1 to 15
+
+// document.write("<b> Counting: </b> ");
+// for (var i = 0; i <= 15; i++) {
+//   document.write(i);
+//   if (i <= 14) {
+//     document.write(" , ");
+//   }
+// }
+
+// Reverse Counting 15 to 1
+
+// document.write("<br/> <b> Reverse Counting:</b> ");
+// for (var i = 15; i >= 1; i--) {
+//   document.write(i);
+//   if (i >= 2) {
+//     document.write(" , ");
+//   }
+// }
+
+// if (b !== "Available") {
+//     document.write("We're sorry not available in our bakery ");
+// }
+
+// Write a program to identify the Largest number in the given array.
+
+// var a = [24, 53, 78, 91, 12];
+
+// document.write("<b> Array Items </b> :  " + a);
+
+// var largest = a[0];
+
+// for (var i = 0; i < a.length; i++) {
+
+//     if (a[i] > largest) {
+//         largest = a[i];
+//     }
+
+// }
+
+// document.write("</br><b> The Largest number is </b> :  " + largest);
+
+
+// Write a program to identify the smallest number in the given array.
+
+// var a = [24, 53, 78, 91, 12];
+
+// document.write("<b> Array Items </b> :  " + a);
+
+// var smallest = a[0];
+
+// for (var i = 0; i < a.length; i++) {
+//     if (a[i] < smallest) {
+//         smallest = a[i];
+//     }
+// }
+
+// document.write("</br> <b> The Smallest Number is </b> :  " + smallest);
+
+// Write a program to print multiples of 2 ranging 1 to 200. 
+
+// for(var i=1; i<=100; i++){
+    
+//     var res = 2*i;
+
+//     document.write(res + "  ")
+// }
